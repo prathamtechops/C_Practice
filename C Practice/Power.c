a41495290ce62c092235bd137e8e98d7f82024b7
Pratham-Techops <88659230+Pratham-Techops@users.noreply.github.com>
2022-01-13 21:35:31 +0530

    C

---
+++ C Practice/Power.c
@@ -0,0 +1,13 @@
+   #include<stdio.h>
+      #include<math.h>
+      int main()
+      {
+        int base,power,d;
+        printf("Enter value of base = ");
+        scanf("%d",&base);
+        printf("Enter value of power = ");
+        scanf("%d",&power);
+        d=pow(base,power);
+        printf("%d",d);
+        return 0;
+      }

4a23db3178274b0eadc3600d7024a772a3909589
Pratham-Techops <88659230+Pratham-Techops@users.noreply.github.com>
2022-01-17 11:54:06 +0530

    Create Cosine_Series.c

---
+++ C Practice/Cosine_Series.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+float formula(float);
+double factorial(int);
+int main()
+{
+    int degree; 
+    float radian;
+    float result;
+    printf("Enter the value of x in degrees: ");
+    scanf("%d", &degree);
+    radian = degree * (3.14159 / 180.0);
+    printf("Cos(%d) = %.3f\n", degree, formula(radian));
+    return 0;
+}
+double factorial(int num){
+    double factorial = 1;
+    for(int i = 1; i <= num; i++){
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+float formula(float radian){
+    float result = 0;
+    int n = 0;
+    int sign = 1;
+    for(int i = 0; n < 10; i += 2){
+         result = result + sign * (pow(radian, i) / factorial(i)) ;
+         n++;
+         sign *= -1;
+    }
+    return result;
+}

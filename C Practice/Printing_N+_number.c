507de0019d8b4ecd1c65f4fef0239b12a89895a6
Pratham-Techops <88659230+Pratham-Techops@users.noreply.github.com>
2022-01-17 12:56:02 +0530

    Re

--- C Practice/Printing_N+_number.c
+++ C Practice/Printing_N+_number.c
@@ -1,24 +1,29 @@
 //Program to print the following series for N given number 1+(1+2)+(1+2+3)+(1+2+3+4)……… 
 #include <stdio.h>
-int result(int);
+int output(int);
 int main()
 {
     int num;
     printf("Enter a number: ");
     scanf("%d", &num);
-    result(num);
+    output(num);
     return 0;
 }
-int result(int number)
+int output(int number)
 {
     int result = 0;
     for(int i = 1; i <= number; i++){
+           printf("(");
         for (int j = 1; j <= i; j++)
         {
-           result += j;
-        }
+           printf("%d", j);
+           if (i == j) printf(")"); 
+           else printf("+");
+        }  
+        if(i==number) printf("");
+        else printf("+");
     }
-    printf("%d", result);
+    
 
 }
 

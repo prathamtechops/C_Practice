1947a729250d7dd017d1c9fdd13dfaedd0950723
Pratham-Techops <88659230+Pratham-Techops@users.noreply.github.com>
2022-01-20 08:17:02 +0530

    Update Palindrome.c

--- C Practice/Palindrome.c
+++ C Practice/Palindrome.c
@@ -2,6 +2,18 @@
 
 int main(int argc, char const *argv[])
 {
-    int 
+    int number, result = 0, q, rem;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    q = number;
+    while (q!= 0)
+    {
+        rem = q%10;
+        result = result*10 + rem;
+        q = q/10;
+    }
+    if (result == number) printf("Its a pallindrome");
+    else printf("Its not a pallindrome");
     return 0;
 }
+ 

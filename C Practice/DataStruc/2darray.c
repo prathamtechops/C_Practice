db49b9060957bcaf2de27da8dbedecc929f04f50
Pratham-Techops <88659230+Pratham-Techops@users.noreply.github.com>
2022-02-03 18:08:09 +0530

    C

---
+++ C Practice/DataStruc/2darray.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a[2][3] = {{1, 2,3}, {4, 5, 6}};
+    // printf("%d ", a[0][0]);
+    for (int i = 0; i < 2; i++){
+        for(int j = 0; j < 3; j++){
+            printf("%d ", a[i][j]);
+        }
+    }
+    return 0;
+}

fe896a76f3f58a506dd4446f1e46ae12b8545a7c
Pratham-Techops <88659230+Pratham-Techops@users.noreply.github.com>
2022-02-12 19:00:01 +0530

    S

--- C Practice/Pattern/MirrorTriangle.c
+++ C Practice/Pattern/MirrorTriangle.c
@@ -4,9 +4,9 @@ int main(int argc, char const *argv[])
    int rows;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
-   for (int i = 1; i <= rows; i++){
+   for (int i = 1; i <= rows ; i++){
        for (int j = 1; j <= rows; j++){
-           if((i + j) <= rows) printf(" ");
+           if((i +j) <= rows) printf(" ");
            else printf("*");
        }
        printf("\n");

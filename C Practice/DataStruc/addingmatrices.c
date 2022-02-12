bdc0a21e6ca5c3809b621e350cf28cd15b4139d0
Pratham-Techops <88659230+Pratham-Techops@users.noreply.github.com>
2022-02-12 15:32:22 +0530

    S

--- C Practice/DataStruc/addingmatrices.c
+++ C Practice/DataStruc/addingmatrices.c
@@ -5,33 +5,33 @@ int main()
     int add[Max][Max];
     int sum = 0;
     int a[Max][Max];
-    int arows, bcolumns, acolumns, brows;
+    int rows, columns;
     int b[Max][Max];
-    printf("Enter the number of rows and columns of matrix a: ");
-    scanf("%d %d", &arows, &acolumns);
-    printf("Enter the number of rows and columns of matrix b: ");
-    scanf("%d %d", &brows, &bcolumns);
-    if(arows != brows || acolumns != bcolumns) printf("Addintion of matrix a and b is not possible\n");
-    else{
-        printf("Enter the elements of the matrix a\n");
-        for(int i = 0; i < arows; i++){
-            for(int j = 0; j < acolumns; j++){
+    printf("Enter the number of rows and columns of matrix a and b: ");
+    scanf("%d %d", &rows, &columns);
+    
+         printf("Enter the elements of the matrix a\n");
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j <columns; j++){
                 scanf("%d", &a[i][j]);
             }
         }
         printf("Enter the elements of the matrix b\n");
-        for (int i = 0; i < brows; i++){
-            for(int j = 0; j < bcolumns; j++){scanf("%d", b[i][j]);
+        for (int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                scanf("%d", b[i][j]);
             }
         }
-        for(int i = 0; i < arows; i++){
-            for(int j = 0; j < bcolumns; j++){
-                add[i][j] = a[i][j] + b[i][j];
-                printf("%d ",add[i][j]);
+        for(int r = 0; r < rows; r++){
+            for(int c = 0; c < columns; c++){
+                add[r][c] = a[r][c] + b[r][c];
+            }
+        }
+        for(int r = 0; r < rows; r++){
+            for(int c = 0; c < columns; c++){
+                printf("%d", add[r][c]);
             }
             printf("\n");
         }
-
-    }
     return 0;
 }
